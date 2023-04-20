c9365596cbb3d00bbf3b25e2136472defe3ae8de
blazte <blazte.aster@gmail.com>
2023-04-20 14:30:41 -0500

    add color to heading component

--- src/components/atoms/Heading/Heading.tsx
+++ src/components/atoms/Heading/Heading.tsx
@@ -5,14 +5,26 @@ import styles from "./Heading.module.scss";
 interface HeadingProps {
   level?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
   size?: "small" | "medium" | "large";
+  color?: "primary" | "secondary" | "white";
   children: React.ReactNode;
 }
 
-export const Heading = ({ level = "h1", size = "small" }: HeadingProps) => {
+export const Heading = ({
+  level = "h1",
+  size = "small",
+  color = "primary",
+  children,
+}: HeadingProps) => {
   const Tag = level;
   return (
-    <Tag className={classNames(styles.heading, styles[`heading--${size}`])}>
-      Heading
+    <Tag
+      className={classNames(
+        styles.heading,
+        styles[`heading--${size}`],
+        styles[`heading--${color}`]
+      )}
+    >
+      {children}
     </Tag>
   );
 };
