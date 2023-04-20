2590a5f6f76e5bc3a3eeee3a40a5048d0e1fd647
blazte <blazte.aster@gmail.com>
2023-04-20 10:05:21 -0500

    add icon outline and fill class

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -1,19 +1,27 @@
 import React, { ReactNode } from "react";
 import styles from "./Button.module.scss";
 import * as classNames from "classnames";
-interface Props {
-  color: "primary" | "secondary";
-  size: "small" | "medium";
+
+export interface ButtonProps {
+  color?: "primary" | "secondary" | "white";
+  size?: "small" | "medium" | "icon";
   children: ReactNode;
+  type?: "fill" | "outline";
 }
 
-export const Button = ({ color, size, children }: Props) => {
+export const Button = ({
+  color = "primary",
+  size = "small",
+  children,
+  type = "fill",
+}: ButtonProps) => {
   return (
     <button
       className={classNames(
         styles.btn,
         styles[`btn--${color}`],
-        styles[`btn--${size}`]
+        styles[`btn--${size}`],
+        styles[`btn--${type}`]
       )}
     >
       {children}
