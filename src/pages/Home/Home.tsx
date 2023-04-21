822aa54ded22b29698fea47da9762455175d94f8
blazte <blazte.aster@gmail.com>
2023-04-21 09:02:35 -0500

    add Header and Footer to App

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+import { Footer } from "@/components/common/Footer/Footer";
+import { Header } from "@/components/common/Header/Header";
+
+export const Home = () => {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+};
