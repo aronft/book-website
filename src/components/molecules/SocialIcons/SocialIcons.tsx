4bfee72a979004c4e61b7a8617761bbab7ccd816
blazte <blazte.aster@gmail.com>
2023-04-21 09:00:38 -0500

    add position to Social Icons component

--- src/components/molecules/SocialIcons/SocialIcons.tsx
+++ src/components/molecules/SocialIcons/SocialIcons.tsx
@@ -5,27 +5,43 @@ import styles from "./SocialIcons.module.scss";
 
 interface SocialIcons extends Pick<ButtonProps, "color" | "type"> {
   iconColor: string;
+  position?: "header" | "footer";
 }
 
 export const SocialIcons = ({
   color = "primary",
   type = "outline",
   iconColor,
+  position = "footer",
 }: SocialIcons) => {
   return (
     <div className={styles["social-icons"]}>
       <Button color={color} size="icon" type={type}>
-        <BsTwitter color={iconColor} size="21px" />
+        <BsTwitter
+          color={iconColor}
+          size={position === "footer" ? "18px" : "16px"}
+        />
       </Button>
       <Button color={color} size="icon" type={type}>
-        <FaFacebookF color={iconColor} size="21px" />
+        <FaFacebookF
+          color={iconColor}
+          size={position === "footer" ? "18px" : "16px"}
+        />
       </Button>
       <Button color={color} size="icon" type={type}>
-        <FaLinkedinIn color={iconColor} size="21px" />
+        <FaLinkedinIn
+          color={iconColor}
+          size={position === "footer" ? "18px" : "16px"}
+        />
       </Button>
-      <Button color={color} size="icon" type={type}>
-        <FaInstagram color={iconColor} size="21px" />
-      </Button>
+      {position === "footer" && (
+        <Button color={color} size="icon" type={type}>
+          <FaInstagram
+            color={iconColor}
+            size={position === "footer" ? "18px" : "16px"}
+          />
+        </Button>
+      )}
     </div>
   );
 };
