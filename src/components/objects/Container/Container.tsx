602d7fd35fc430f2022d5387ecb38c606a2e26b4
blazte <blazte.aster@gmail.com>
2023-04-29 21:36:17 -0500

    add className to Container

--- src/components/objects/Container/Container.tsx
+++ src/components/objects/Container/Container.tsx
@@ -1,10 +1,14 @@
 import { ReactNode } from "react";
 import styles from "./Container.module.scss";
+import classNames from "classnames";
 
 interface ContainerProps {
   children: ReactNode;
+  className?: string;
 }
 
-export const Container = ({ children }: ContainerProps) => {
-  return <div className={styles.container}>{children}</div>;
+export const Container = ({ children, className }: ContainerProps) => {
+  return (
+    <div className={classNames(styles.container, className)}>{children}</div>
+  );
 };
