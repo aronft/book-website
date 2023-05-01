62b469026141771490d7c65e1a797b491826168f
blazte <blazte.aster@gmail.com>
2023-05-01 12:20:17 -0500

    create Separator component

---
+++ src/components/atoms/Separator/Separator.tsx
@@ -0,0 +1,25 @@
+import classNames from "classnames";
+import styles from "./Separator.module.scss";
+
+interface SeparatorProps {
+  size?: "small" | "medium" | "large";
+  orientation?: "vertical" | "horizontal";
+  color?: "secondary";
+}
+
+export const Separator = ({
+  size = "medium",
+  orientation = "vertical",
+  color = "secondary",
+}: SeparatorProps) => {
+  return (
+    <div
+      className={classNames(
+        styles.separator,
+        styles[`separator--${size}`],
+        styles[`separator--${orientation}`],
+        styles[`separator--${color}`]
+      )}
+    ></div>
+  );
+};
