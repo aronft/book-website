3395cf74983c1b691ffd2b20a08a8c28b0a6c783
blazte <blazte.aster@gmail.com>
2023-05-03 14:08:22 -0500

    exclude no-unsecaped in html

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -22,6 +22,7 @@ module.exports = {
   plugins: ["react", "@typescript-eslint"],
   rules: {
     "react/react-in-jsx-scope": "off",
+    "react/no-unescaped-entities": 0,
     "prettier/prettier": "error",
   },
 };
