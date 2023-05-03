316ab733a2f7ada83271a502e7b3159b25327bed
blazte <blazte.aster@gmail.com>
2023-05-03 17:21:29 -0500

    create TestimonialCard component

---
+++ src/components/ui/molecules/TestimonialCard/TestimonialCard.tsx
@@ -0,0 +1,25 @@
+import { Avatar } from "../../atoms/Avatar/Avatar";
+import { Rating } from "../../atoms/Rating/Rating";
+import styles from "./TestimonialCard.module.scss";
+
+interface TestimonialCardProps {
+  user: User;
+  title: string;
+  description: string;
+}
+
+export const TestimonialCard = ({ user, title, description }: TestimonialCardProps) => {
+  return (
+    <article className={styles.testimonial}>
+      <div className={styles.user}>
+        <Avatar srcImage={user.profileImage} />
+        <div className={styles.user__info}>
+          <p className={styles.user__name}>{user.name}</p>
+          <Rating totalRating={5} actualRating={4} />
+        </div>
+      </div>
+      <p className={styles.testimonial__title}>“ {title} ”</p>
+      <p className={styles.testimonial__description}>{description}</p>
+    </article>
+  );
+};
