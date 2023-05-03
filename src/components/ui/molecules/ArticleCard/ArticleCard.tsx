1929b2695f3e2412941035ce5042e4943a2ae497
blazte <blazte.aster@gmail.com>
2023-05-03 07:57:45 -0500

    create ArticleCard component

---
+++ src/components/ui/molecules/ArticleCard/ArticleCard.tsx
@@ -0,0 +1,30 @@
+import { Button } from "../../atoms/Button";
+import { Heading, HeadingProps } from "../../atoms/Heading/Heading";
+import styles from "./ArticleCard.module.scss";
+
+interface ArticleCardProps extends Pick<HeadingProps, "level"> {
+  image: string;
+  title: string;
+  description: string;
+  date: string;
+}
+
+export const ArticleCard = ({ image, title, description, date, level }: ArticleCardProps) => {
+  return (
+    <section className={styles["article"]}>
+      <div className={styles["article__image-container"]}>
+        <img src={image} alt={title} />
+      </div>
+      <div className={styles.article__info}>
+        <Heading level={level}>{title}</Heading>
+        <p className={styles["article__description"]}>{description}</p>
+        <footer className={styles["article__footer"]}>
+          <Button href="#" color="primary">
+            Read more
+          </Button>
+          <span>Author - {date}</span>
+        </footer>
+      </div>
+    </section>
+  );
+};
