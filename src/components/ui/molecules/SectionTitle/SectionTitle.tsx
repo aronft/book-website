150f44b18c9d7cc458c67bb407aa2b67d8780dcf
blazte <blazte.aster@gmail.com>
2023-05-04 12:28:14 -0500

    add color to SectioTitle component

--- src/components/ui/molecules/SectionTitle/SectionTitle.tsx
+++ src/components/ui/molecules/SectionTitle/SectionTitle.tsx
@@ -1,10 +1,10 @@
-import { Heading } from "../../atoms/Heading/Heading";
+import { Heading, HeadingProps } from "../../atoms/Heading/Heading";
 import { PropsWithChildren } from "react";
 import { Separator } from "../../atoms/Separator/Separator";
 import styles from "./SectionTitle.module.scss";
 import classNames from "classnames";
 
-interface SectionTitleProps {
+interface SectionTitleProps extends Pick<HeadingProps, "color"> {
   separator?: boolean;
   aligment: "middle" | "left";
 }
@@ -13,10 +13,11 @@ export const SectionTitle = ({
   children,
   separator = false,
   aligment = "middle",
+  color = "primary",
 }: PropsWithChildren<SectionTitleProps>) => {
   return (
     <div className={classNames(styles["section-title"], styles[`section-title--${aligment}`])}>
-      <Heading level="h2" size="medium" color="primary">
+      <Heading level="h2" size="medium" color={color}>
         {children}
       </Heading>
       {separator && <Separator orientation="horizontal" size="medium" color="secondary" />}
