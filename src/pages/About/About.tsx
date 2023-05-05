0c3900ed802c455040964c2ec4465e077e38ee47
blazte <blazte.aster@gmail.com>
2023-05-05 09:00:12 -0500

    import sections and heroInfo to about page

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,5 +1,19 @@
-import React from "react";
+import { AuthorBooksSection } from "@/components/Books/components/AuthorBooksSection/AuthorBooksSection";
+import { ConfidenceSection } from "@/components/common/ConfidenceSection/ConfidenceSection";
+import { HeroInfo } from "@/components/common/HeroInfo/HeroInfo";
+import SubscriptionArticle from "@/components/common/SubscriptionArticle/SubscriptionArticle";
+import stylesColor from "@/styles/settings/colors-map.module.scss";
 
 export const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <HeroInfo title="About the Actor">
+        There are many variations of passages of Lorem Ipsum available, have suffered alteration in
+        some form.
+      </HeroInfo>
+      <AuthorBooksSection color={stylesColor.thertiary} />
+      <ConfidenceSection paddingBot={false} />
+      <SubscriptionArticle />
+    </>
+  );
 };
