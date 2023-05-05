14548c4607bcdfe1f60f0540e88092d45211ddd0
blazte <blazte.aster@gmail.com>
2023-05-05 08:56:59 -0500

    change a element for Link react router dom

--- src/components/common/Menu/Menu.tsx
+++ src/components/common/Menu/Menu.tsx
@@ -1,18 +1,17 @@
 import { Link } from "@/components/ui/atoms/Link/Link";
 import { UiList } from "@/components/ui/objects/UiList/UiList";
-import React from "react";
 
 export const Menu = () => {
   return (
     <div>
       <UiList>
-        <Link href="#" color="white">
+        <Link href="/" color="white">
           Home
         </Link>
-        <Link href="#" color="white">
+        <Link href="/pages" color="white">
           Pages
         </Link>
-        <Link href="#" color="white">
+        <Link href="/about" color="white">
           About
         </Link>
         <Link href="#" color="white">
--- src/components/ui/atoms/Link/Link.tsx
+++ src/components/ui/atoms/Link/Link.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
 import styles from "./Link.module.scss";
 import classNames from "classnames";
+import { Link as LinkRouterDom } from "react-router-dom";
 
 interface LinkProps {
   color?: "primary" | "secondary" | "white";
@@ -9,11 +10,8 @@ interface LinkProps {
 }
 export const Link = ({ href, children, color = "primary" }: LinkProps) => {
   return (
-    <a
-      href={href}
-      className={classNames(styles.link, styles[`link--${color}`])}
-    >
+    <LinkRouterDom to={href} className={classNames(styles.link, styles[`link--${color}`])}>
       {children}
-    </a>
+    </LinkRouterDom>
   );
 };
