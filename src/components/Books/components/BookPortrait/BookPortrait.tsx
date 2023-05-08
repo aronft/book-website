54e464bf7f6f828ba60153f7e7aa43d4a09350db
blazte <blazte.aster@gmail.com>
2023-05-08 16:05:19 -0500

    Change size BookPortrait

--- src/components/Books/components/BookPortrait/BookPortrait.tsx
+++ src/components/Books/components/BookPortrait/BookPortrait.tsx
@@ -1,18 +1,28 @@
+import classNames from "classnames";
 import styles from "./BookPortrait.module.scss";
 
 export interface BookPortraitProps {
   backgroundVisible?: boolean;
   imageSrc: string;
   imageAlt: string;
+  size: "small" | "medium" | "full";
   type: "print" | "digital" | "vinilo";
 }
 export const BookPortrait = ({
   backgroundVisible = false,
   imageSrc,
   imageAlt,
+  size = "small",
 }: BookPortraitProps) => {
+  const paddingPortrait = backgroundVisible ? styles["book-portrait--bg"] : "";
   return (
-    <div className={styles["book-portrait"]}>
+    <div
+      className={classNames(
+        styles["book-portrait"],
+        paddingPortrait,
+        styles[`book-portrait--${size}`]
+      )}
+    >
       <img className={styles["book-porttrait__image"]} src={imageSrc} alt={imageAlt} />
     </div>
   );
