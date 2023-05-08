8efe62c968225bc007d442d45ba4e5e7ebfe879c
blazte <blazte.aster@gmail.com>
2023-05-08 16:07:13 -0500

    change content for Routing with StoreRoutes

--- src/pages/Store/Store.tsx
+++ src/pages/Store/Store.tsx
@@ -1,4 +1,10 @@
+import { BookList } from "@/components/Books/components/BookList/BookList";
+import { useBooksByLimit } from "@/components/Books/hooks/useBooksByLimit";
 import { HeroInfo } from "@/components/common/HeroInfo/HeroInfo";
+import { Container } from "@/components/ui/objects/Container/Container";
+import styles from "./Store.module.scss";
+import SubscriptionArticle from "@/components/common/SubscriptionArticle/SubscriptionArticle";
+import { StoreRoutes } from "./routes/StoreRoutes";
 
 export const Store = () => {
   return (
@@ -7,6 +13,7 @@ export const Store = () => {
         There are many variations of passages of Lorem Ipsum available, have suffered alteration in
         some form.
       </HeroInfo>
+      <StoreRoutes />
     </>
   );
 };
