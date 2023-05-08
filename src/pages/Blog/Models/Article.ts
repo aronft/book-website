16ea81faa68cee5c4e986a081aecc9fbd01856f7
blazte <blazte.aster@gmail.com>
2023-05-07 22:05:04 -0500

    create SubContent interface and add into article data

--- src/pages/Blog/Models/Article.ts
+++ src/pages/Blog/Models/Article.ts
@@ -1,7 +1,16 @@
+interface SubContent {
+  id: string;
+  quote: string;
+  title: string;
+  description: string;
+  list: string[];
+}
+
 export interface Article {
   id: string;
   image: string;
   title: string;
   description: string;
   date: string;
+  subContent: SubContent[];
 }
---
+++ src/pages/Blog/data/articles.ts
@@ -0,0 +1,258 @@
+import image1 from "@/assets/images/articles/girl-reading.png";
+import image2 from "@/assets/images/articles/book.png";
+import image3 from "@/assets/images/articles/women-reading.png";
+import image4 from "@/assets/images/articles/pexels-pew-nguyen.png";
+import image5 from "@/assets/images/articles/pexels-andrea-piacquadio.png";
+import image6 from "@/assets/images/articles/pexels-diana-agapova.png";
+import image7 from "@/assets/images/articles/pexels-karolina-grabowska.png";
+import image8 from "@/assets/images/articles/pexels-pew-nguyen.png";
+import image9 from "@/assets/images/articles/sincerely-media.png";
+import { uuiGenerator } from "@/utils/uuidGenerator";
+import { Article } from "../Models/Article";
+
+export const ARTICLES: Article[] = [
+  {
+    id: "2e1db617-f04f-4ca2-8690-6b0ec1ac9b57",
+    image: image1,
+    title: "Significant reading has more info number",
+    description:
+      "The point of using Lorem Ipsum hiter of that using making it look like others readable will get end.",
+    date: "23.05.2022",
+    subContent: [
+      {
+        id: "2e1db617-f04f-4ca2-8690-6b0ec1ac9b57",
+        title: "The perfect book for all",
+        description:
+          "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has less normal distribution of letters, as opposed.",
+        quote:
+          "Crowdsource term sheet freemium beta stealth responsive web design niche market rock star channels.",
+        list: [
+          "It has roots in a piece of classical Latin literature from 45 BC,",
+          "To generate Lorem Ipsum which looks reasonable",
+          'The first line of Lorem Ipsum, "Lorem ipsum” dolor sit amet',
+        ],
+      },
+      {
+        id: "2e1db617-f04f-4ca2-8690-6b0ec1ac9b67",
+        title: "The perfect book for all",
+        description:
+          "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has less normal distribution of letters, as opposed.",
+        quote:
+          "Crowdsource term sheet freemium beta stealth responsive web design niche market rock star channels.",
+        list: [
+          "It has roots in a piece of classical Latin literature from 45 BC,",
+          "To generate Lorem Ipsum which looks reasonable",
+          'The first line of Lorem Ipsum, "Lorem ipsum” dolor sit amet',
+        ],
+      },
+    ],
+  },
+  {
+    id: "70c46308-3dbc-4680-ad20-e05409ae6589",
+    image: image2,
+    title: "Release of Letraset sheets tools containing  passages",
+    description:
+      "The point of using Lorem Ipsum hiter of that using making it look like others readable will get end.",
+    date: " 23.05.2022",
+    subContent: [
+      {
+        id: "2e1db617-f04f-4ca2-8690-6b0ec1ac9b57",
+        title: "The perfect book for all",
+        description:
+          "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has less normal distribution of letters, as opposed.",
+        quote:
+          "Crowdsource term sheet freemium beta stealth responsive web design niche market rock star channels.",
+        list: [
+          "It has roots in a piece of classical Latin literature from 45 BC,",
+          "To generate Lorem Ipsum which looks reasonable",
+          'The first line of Lorem Ipsum, "Lorem ipsum” dolor sit amet',
+        ],
+      },
+    ],
+  },
+  {
+    id: "8d6e251c-bb35-4b42-8a06-5d5e95b856df",
+    image: image3,
+    title: "Release of Letraset sheets tools containing  passages",
+    description:
+      "The point of using Lorem Ipsum hiter of that using making it look like others readable will get end.",
+    date: " 23.05.2022",
+    subContent: [
+      {
+        id: "2e1db617-f04f-4ca2-8690-6b0ec1ac9b57",
+        title: "The perfect book for all",
+        description:
+          "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has less normal distribution of letters, as opposed.",
+        quote:
+          "Crowdsource term sheet freemium beta stealth responsive web design niche market rock star channels.",
+        list: [
+          "It has roots in a piece of classical Latin literature from 45 BC,",
+          "To generate Lorem Ipsum which looks reasonable",
+          'The first line of Lorem Ipsum, "Lorem ipsum” dolor sit amet',
+        ],
+      },
+    ],
+  },
+  {
+    id: "1802e0b0-e756-4e4b-9dc4-01ea5998e56c",
+    image: image4,
+    title: "Release of Letraset sheets tools containing  passages",
+    description:
+      "The point of using Lorem Ipsum hiter of that using making it look like others readable will get end.",
+    date: " 23.05.2022",
+    subContent: [
+      {
+        id: "2e1db617-f04f-4ca2-8690-6b0ec1ac9b57",
+        title: "The perfect book for all",
+        description:
+          "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has less normal distribution of letters, as opposed.",
+        quote:
+          "Crowdsource term sheet freemium beta stealth responsive web design niche market rock star channels.",
+        list: [
+          "It has roots in a piece of classical Latin literature from 45 BC,",
+          "To generate Lorem Ipsum which looks reasonable",
+          'The first line of Lorem Ipsum, "Lorem ipsum” dolor sit amet',
+        ],
+      },
+    ],
+  },
+  {
+    id: "ee96e066-af3c-4310-9df1-1744eeaa1612",
+    image: image5,
+    title: "Release of Letraset sheets tools containing  passages",
+    description:
+      "The point of using Lorem Ipsum hiter of that using making it look like others readable will get end.",
+    date: " 23.05.2022",
+    subContent: [
+      {
+        id: "2e1db617-f04f-4ca2-8690-6b0ec1ac9b57",
+        title: "The perfect book for all",
+        description:
+          "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has less normal distribution of letters, as opposed.",
+        quote:
+          "Crowdsource term sheet freemium beta stealth responsive web design niche market rock star channels.",
+        list: [
+          "It has roots in a piece of classical Latin literature from 45 BC,",
+          "To generate Lorem Ipsum which looks reasonable",
+          'The first line of Lorem Ipsum, "Lorem ipsum” dolor sit amet',
+        ],
+      },
+    ],
+  },
+  {
+    id: "ce491bf2-7378-4375-b798-a97b835a7cf9",
+    image: image6,
+    title: "Release of Letraset sheets tools containing  passages",
+    description:
+      "The point of using Lorem Ipsum hiter of that using making it look like others readable will get end.",
+    date: " 23.05.2022",
+    subContent: [
+      {
+        id: "2e1db617-f04f-4ca2-8690-6b0ec1ac9b57",
+        title: "The perfect book for all",
+        description:
+          "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has less normal distribution of letters, as opposed.",
+        quote:
+          "Crowdsource term sheet freemium beta stealth responsive web design niche market rock star channels.",
+        list: [
+          "It has roots in a piece of classical Latin literature from 45 BC,",
+          "To generate Lorem Ipsum which looks reasonable",
+          'The first line of Lorem Ipsum, "Lorem ipsum” dolor sit amet',
+        ],
+      },
+    ],
+  },
+
+  {
+    id: "23085bd5-992a-4b14-b54e-99d123e264c6",
+    image: image7,
+    title: "Release of Letraset sheets tools containing  passages",
+    description:
+      "The point of using Lorem Ipsum hiter of that using making it look like others readable will get end.",
+    date: " 23.05.2022",
+    subContent: [
+      {
+        id: "2e1db617-f04f-4ca2-8690-6b0ec1ac9b57",
+        title: "The perfect book for all",
+        description:
+          "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has less normal distribution of letters, as opposed.",
+        quote:
+          "Crowdsource term sheet freemium beta stealth responsive web design niche market rock star channels.",
+        list: [
+          "It has roots in a piece of classical Latin literature from 45 BC,",
+          "To generate Lorem Ipsum which looks reasonable",
+          'The first line of Lorem Ipsum, "Lorem ipsum” dolor sit amet',
+        ],
+      },
+    ],
+  },
+  {
+    id: "c9f5b19f-01fd-4564-9520-5cc4f534eedf",
+    image: image8,
+    title: "Release of Letraset sheets tools containing  passages",
+    description:
+      "The point of using Lorem Ipsum hiter of that using making it look like others readable will get end.",
+    date: " 23.05.2022",
+    subContent: [
+      {
+        id: "2e1db617-f04f-4ca2-8690-6b0ec1ac9b57",
+        title: "The perfect book for all",
+        description:
+          "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has less normal distribution of letters, as opposed.",
+        quote:
+          "Crowdsource term sheet freemium beta stealth responsive web design niche market rock star channels.",
+        list: [
+          "It has roots in a piece of classical Latin literature from 45 BC,",
+          "To generate Lorem Ipsum which looks reasonable",
+          'The first line of Lorem Ipsum, "Lorem ipsum” dolor sit amet',
+        ],
+      },
+    ],
+  },
+  {
+    id: "7a381bfc-1e99-4f28-8c9c-b50d87944cde",
+    image: image9,
+    title: "Release of Letraset sheets tools containing  passages",
+    description:
+      "The point of using Lorem Ipsum hiter of that using making it look like others readable will get end.",
+    date: " 23.05.2022",
+    subContent: [
+      {
+        id: "2e1db617-f04f-4ca2-8690-6b0ec1ac9b57",
+        title: "The perfect book for all",
+        description:
+          "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has less normal distribution of letters, as opposed.",
+        quote:
+          "Crowdsource term sheet freemium beta stealth responsive web design niche market rock star channels.",
+        list: [
+          "It has roots in a piece of classical Latin literature from 45 BC,",
+          "To generate Lorem Ipsum which looks reasonable",
+          'The first line of Lorem Ipsum, "Lorem ipsum” dolor sit amet',
+        ],
+      },
+    ],
+  },
+  {
+    id: "46d343e5-2dec-4929-9b32-d8f8c69494ee",
+    image: image6,
+    title: "Release of Letraset sheets tools containing  passages",
+    description:
+      "The point of using Lorem Ipsum hiter of that using making it look like others readable will get end.",
+    date: " 23.05.2022",
+    subContent: [
+      {
+        id: "2e1db617-f04f-4ca2-8690-6b0ec1ac9b57",
+        title: "The perfect book for all",
+        description:
+          "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has less normal distribution of letters, as opposed.",
+        quote:
+          "Crowdsource term sheet freemium beta stealth responsive web design niche market rock star channels.",
+        list: [
+          "It has roots in a piece of classical Latin literature from 45 BC,",
+          "To generate Lorem Ipsum which looks reasonable",
+          'The first line of Lorem Ipsum, "Lorem ipsum” dolor sit amet',
+        ],
+      },
+    ],
+  },
+];
