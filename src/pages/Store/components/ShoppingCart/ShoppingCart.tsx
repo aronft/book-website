0963b1dbe86e987389ccad5bfa42dc91fc564fb3
blazte <blazte.aster@gmail.com>
2023-05-12 07:39:01 -0500

    handle no items in shopping cart

--- src/pages/Store/components/ShoppingCart/ShoppingCart.tsx
+++ src/pages/Store/components/ShoppingCart/ShoppingCart.tsx
@@ -12,30 +12,36 @@ export const ShoppingCart = () => {
   const { items, getTotal } = useShoppingStore();
   const { isOpen, onClose } = useModalStore();
   return (
-    <>
-      <Modal isOpen={isOpen} title="Shopping Cart" onClose={onClose}>
-        <div className={styles["shopping-cart"]}>
-          <div className={styles["shopping-cart__items"]}>
-            {items.map((item, index) => (
-              <ItemCart key={index} book={item.book} quantity={item.quantity} />
-            ))}
-          </div>
-          <hr />
-          <div className={styles["shopping-cart__info"]}>
-            <Text tag="p" color="primary" fontFamily="secondary" size="md">
-              Sub-Total
-            </Text>
-            <Text tag="span" weight={800} color="primary">
-              {getTotal()} usd
-            </Text>
-          </div>
-          <div className={styles["shopping-cart__footer"]}>
-            <Button color="secondary" colorText="primary">
-              Continue to Checkout
-            </Button>
-          </div>
-        </div>
-      </Modal>
-    </>
+    <Modal isOpen={isOpen} title="Shopping Cart" onClose={onClose}>
+      <div className={styles["shopping-cart"]}>
+        {items.length === 0 ? (
+          <Text tag="p" color="thertiary">
+            No items found
+          </Text>
+        ) : (
+          <>
+            <div className={styles["shopping-cart__items"]}>
+              {items.map((item, index) => (
+                <ItemCart key={index} book={item.book} quantity={item.quantity} />
+              ))}
+            </div>
+            <hr />
+            <div className={styles["shopping-cart__info"]}>
+              <Text tag="p" color="primary" fontFamily="secondary" size="md">
+                Sub-Total
+              </Text>
+              <Text tag="span" weight={800} color="primary">
+                {getTotal()} usd
+              </Text>
+            </div>
+            <div className={styles["shopping-cart__footer"]}>
+              <Button color="secondary" colorText="primary">
+                Continue to Checkout
+              </Button>
+            </div>
+          </>
+        )}
+      </div>
+    </Modal>
   );
 };
