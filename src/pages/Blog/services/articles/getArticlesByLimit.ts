0817d97569deab642484efb701993dc0c1afb424
blazte <blazte.aster@gmail.com>
2023-05-12 09:56:01 -0500

    change logo icon

--- src/pages/Blog/services/articles/getArticlesByLimit.ts
+++ src/pages/Blog/services/articles/getArticlesByLimit.ts
@@ -3,6 +3,5 @@ import { ARTICLES } from "../../data/articles";
 
 export const getArticlesByLimit = (limit: number): Article[] => {
   const articles = ARTICLES.filter((article, index) => index < limit);
-  console.log(articles);
   return articles;
 };
