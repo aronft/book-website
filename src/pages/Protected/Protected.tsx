bd94b25e85ecc08f396d685d0fc601e602685d2d
blazte <blazte.aster@gmail.com>
2023-05-16 21:45:05 -0500

    add mobile version to protected page

--- src/pages/Protected/Protected.tsx
+++ src/pages/Protected/Protected.tsx
@@ -4,6 +4,7 @@ import { SectionTitle } from "@/components/ui/molecules/SectionTitle/SectionTitl
 import { FiKey } from "react-icons/fi";
 import styles from "./Protected.module.scss";
 import { Input } from "@/components/ui/atoms/Input/Input";
+import { Container } from "@/components/ui/objects/Container/Container";
 
 export const Protected = () => {
   return (
@@ -12,24 +13,25 @@ export const Protected = () => {
         There are many variations of passages of Lorem Ipsum available, have suffered alteration in
         some form.
       </HeroInfo>
-
-      <section className={styles.protected}>
-        <div className={styles.protected__content}>
-          <SectionTitle separator color="primary" aligment="middle">
-            Password Protected
-          </SectionTitle>
-          <p className={styles.protected__description}>
-            This page is password protected. If you are the website admin, or have access to this
-            page, please type your password below.
-          </p>
-          <Input placeHolder="Enter your password" type="password">
-            <FiKey />
-          </Input>
-          <Button color="secondary" size="medium" colorText="primary">
-            Submit
-          </Button>
-        </div>
-      </section>
+      <Container>
+        <section className={styles.protected}>
+          <div className={styles.protected__content}>
+            <SectionTitle separator color="primary" aligment="middle">
+              Password Protected
+            </SectionTitle>
+            <p className={styles.protected__description}>
+              This page is password protected. If you are the website admin, or have access to this
+              page, please type your password below.
+            </p>
+            <Input placeHolder="Enter your password" type="password">
+              <FiKey />
+            </Input>
+            <Button color="secondary" size="medium" colorText="primary">
+              Submit
+            </Button>
+          </div>
+        </section>
+      </Container>
     </>
   );
 };
