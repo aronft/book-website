8daa55a3a94260441c85559232dadbb28e7e2b9e
blazte <blazte.aster@gmail.com>
2023-05-17 17:35:16 -0500

    add mobile version to store page and single store page

--- src/pages/Store/Store.tsx
+++ src/pages/Store/Store.tsx
@@ -1,9 +1,4 @@
-import { BookList } from "@/components/Books/components/BookList/BookList";
-import { useBooksByLimit } from "@/components/Books/hooks/useBooksByLimit";
 import { HeroInfo } from "@/components/common/HeroInfo/HeroInfo";
-import { Container } from "@/components/ui/objects/Container/Container";
-import styles from "./Store.module.scss";
-import SubscriptionArticle from "@/components/common/SubscriptionArticle/SubscriptionArticle";
 import { StoreRoutes } from "./routes/StoreRoutes";
 
 export const Store = () => {
