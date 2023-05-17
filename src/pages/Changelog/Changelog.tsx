85987c1af574f4044cc2f672a225998d9049dec1
blazte <blazte.aster@gmail.com>
2023-05-16 21:40:59 -0500

    add mobile version to changelog page

--- src/pages/Changelog/Changelog.tsx
+++ src/pages/Changelog/Changelog.tsx
@@ -1,5 +1,6 @@
 import { HeroInfo } from "@/components/common/HeroInfo/HeroInfo";
 import styles from "./Changelog.module.scss";
+import { Container } from "@/components/ui/objects/Container/Container";
 
 export const Changelog = () => {
   return (
@@ -8,12 +9,14 @@ export const Changelog = () => {
         There are many variations of passages of Lorem Ipsum available, have suffered alteration in
         some form.
       </HeroInfo>
-      <section className={styles.changelog}>
-        <div className={styles.changelog__content}>
-          <div className={styles.changelog__version}>V.1</div>
-          <p className={styles.changelog__description}>Initial Pages Webflow Template Release</p>
-        </div>
-      </section>
+      <Container>
+        <section className={styles.changelog}>
+          <div className={styles.changelog__content}>
+            <div className={styles.changelog__version}>V.1</div>
+            <p className={styles.changelog__description}>Initial Pages Webflow Template Release</p>
+          </div>
+        </section>
+      </Container>
     </>
   );
 };
